de4fdec4400aebbf4203ea90b404dcdd34ab3cd1
wolf-dagger <cu.17bcs4556@gmail.com>
2022-05-15 01:04:59 +0530

    Adding the project

---
+++ script.js
@@ -0,0 +1,18 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradiant");
+
+
+function setGrad(){
+    body.style.background = 
+    "linear-gradient(to right,"
+    + color1.value 
+    + ","+ color2.value 
+    + ")";
+
+}
+
+color1.addEventListener("input", setGrad);
+
+color2.addEventListener("input", setGrad);
